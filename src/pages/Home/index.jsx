2ae15f613a06a71ca776c32ea31eb731c34ec346
Fellipe <fellipealbert3@gmail.com>
2023-02-14 08:44:05 -0300

    add a filter to search post cards

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,8 @@
 import { Component } from "react";
 
-import { PostCard } from "../../components/Post/PostCard";
-import { MorePostsButton } from "../../components/MorePostsButton";
+import { PostFilters } from "../../components/PostManagement/PostFilters";
+import { PostCard } from "../../components/PostManagement/Post/PostCard";
+import { MorePostsButton } from "../../components/PostManagement/MorePostsButton";
 import { getPostsAndPhotosFromApi } from "../../utils/generics";
 
 import "./styles.css";
@@ -12,11 +13,17 @@ export class Home extends Component {
     allPosts: [],
     page: 0,
     postsPerPage: 2,
+    searchValue: "",
   };
 
   componentDidMount() {
     this.loadPosts();
   }
+
+  handleChange = (e) => {
+    const { value } = e.target;
+    this.setState({ searchValue: value });
+  };
 
   loadPosts = async () => {
     const { page, postsPerPage } = this.state;
@@ -38,18 +45,40 @@ export class Home extends Component {
   };
 
   render() {
-    const { posts, page, postsPerPage, allPosts } = this.state;
+    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
     const noMorePosts = page + postsPerPage >= allPosts.length;
+
+    const filteredPosts = !!searchValue
+      ? allPosts.filter((post) => {
+          return post.title.toLowerCase().includes(searchValue.toLowerCase());
+        })
+      : posts;
 
     return (
       <section className="container">
-        <PostCard posts={posts} />
-        <div className="button-container">
-          <MorePostsButton
-            text={"Mais cards"}
-            onClick={this.loadMorePosts}
-            disabled={noMorePosts}
+        <div className="search-container">
+          {!!searchValue && <h1>Search value: {searchValue}</h1>}
+
+          <PostFilters
+            searchValue={searchValue}
+            handleChange={this.handleChange}
           />
+        </div>
+
+        {filteredPosts.length > 0 && <PostCard posts={filteredPosts} />}
+
+        {filteredPosts.length === 0 && (
+          <p>Não existem posts para o termo pesquisado.</p>
+        )}
+
+        <div className="button-container">
+          {!searchValue && (
+            <MorePostsButton
+              text="Load more posts"
+              onClick={this.loadMorePosts}
+              disabled={noMorePosts}
+            />
+          )}
         </div>
       </section>
     );
